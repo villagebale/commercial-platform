91dfab46c1a180cc00ba980d515a49b6839aa0c1
villagebale <balebu112@gmail.com>
2022-05-02 13:56:13 +0800

    updating route components and adding new fonts

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import { Outlet, Link} from "react-router-dom";
+import { Fragment } from "react";
+import {ReactComponent as InfiLogo} from '../../assets/infinity.svg'
+import './navigation.styles.scss'
+
+const Navigation = () => {
+    return (
+        <Fragment>
+            <div className="navigation">
+                <Link className="logo-container" to='/'>
+                    <InfiLogo />
+                </Link>
+                <div className="nav-links-container">
+                    <Link className="nav-link" to='/shop'>
+                        Shop
+                    </Link>
+                </div>
+            </div>
+            <Outlet />
+        </Fragment>
+    )
+}
+
+export default Navigation;
