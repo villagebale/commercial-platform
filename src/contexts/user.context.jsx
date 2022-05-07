550e8fe1a975e29b1346ebf3c92942c91dcfed08
villagebale <balebu112@gmail.com>
2022-05-07 16:07:31 +0800

    changes in cart-dropdown scroll and test of reducer in user context

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,25 +1,55 @@
-import { createContext, useState, useEffect } from "react";
-import { onAuthStateChangedListener,createUsersDocumentFromAuth} from "../utils/firebase/firebase.utils";
+import { createContext, useEffect, useReducer } from "react";
+import { onAuthStateChangedListener, createUsersDocumentFromAuth } from "../utils/firebase/firebase.utils";
 
 export const UserContext = createContext({
     currentUser: null,
-    setCurrentUser:() => null,
+    setCurrentUser: () => null,
 })
 
-export const UserProvider = ({children}) => {
-    const [currentUser,setCurrentUser] = useState(null);
-    const value = {currentUser, setCurrentUser};
+export const USER_ACTION_TYPES =  {
+    SET_CURRENT_USER : 'SET_CURRENT_USER'
+}
 
-    useEffect(()=>{
+
+
+const userReducer = (state, action) => {
+    const { type, payload } = action;
+
+    switch(type) {
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return {
+                ...state,
+                currentUser: payload
+            }
+        default:
+            throw new Error(`unhandled type ${type} in userReducer`);
+    }
+}
+
+const INITIAL_STATE = {
+    currentUser : null
+}
+
+export const UserProvider = ({ children }) => {
+    // const [currentUser, setCurrentUser] = useState(null);
+    const [state, dispatch] = useReducer(userReducer, INITIAL_STATE)
+
+    const setCurrentUser = (user) => {
+        dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user})
+    } 
+
+    const value = { currentUser, setCurrentUser };
+
+    useEffect(() => {
         const unsubcribe = onAuthStateChangedListener((user) => {
-            if(user) {
-                createUsersDocumentFromAuth(user); 
+            if (user) {
+                createUsersDocumentFromAuth(user);
             }
             setCurrentUser(user);
         });
 
         return unsubcribe;
-    },[])
+    }, [])
 
     return (
         <UserContext.Provider value={value}>{children}</UserContext.Provider>
